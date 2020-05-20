bc20e346bb668ab62f516d5fbf5f1ad73d685876
Jay <jayshahx01@gmail.com>
2020-05-20 23:27:53 +0530

    Third.java  - By Jay

---
+++ app/src/main/java/com/bvm/ui_example_4/Teacher_assignment.java
@@ -0,0 +1,14 @@
+package com.bvm.ui_example_4;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Teacher_assignment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_assignment);
+    }
+}
